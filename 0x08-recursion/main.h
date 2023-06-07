0ad218bcdd89fa00642e12a03e59e1735d35301c
ericbigalito <emugarz@gmail.com>
2023-06-07 12:09:31 -0700

    Task100 updated

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -8,6 +5,8 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int omar_needs_this(int n, int s);
 int is_prime_number(int n);
-
-#endif
+int omar_prime_fun(int n, int s);
+int is_palindrome(char *s);
+int omar_palindrome(char *s, int i);
